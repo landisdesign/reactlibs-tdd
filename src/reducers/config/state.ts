6f8e111da61494e810b6875d04adaa09a488e1ac
Michael Landis <michael4css@gmail.com>
2020-01-06 23:10:03 -0800

    Simplify Config State

---
+++ src/reducers/config/state.ts
@@ -0,0 +1,43 @@
+export interface ConfigState {
+    readonly loaded: boolean;
+    readonly loading: boolean;
+    readonly storySources: StorySource;
+    readonly wordSources: WordSource[];
+}
+
+export type StorySource = StoryList | Config;
+export type WordSource = Word | Config;
+
+export interface Config {
+    readonly loaded: boolean;
+    readonly url: string;
+}
+
+export interface StoryList {
+    readonly loaded: boolean;
+    readonly stories: Story[];
+}
+
+export interface Story {
+    readonly id: string;
+    readonly title: string;
+    readonly fields: string[];
+    readonly template: string;
+}
+
+export type Word = WordRef | WordList;
+
+export interface WordRef extends WordBase {
+    readonly ref: string;
+}
+
+export interface WordList extends WordBase {
+    readonly words: string[];
+}
+
+interface WordBase {
+	readonly loaded: boolean;
+	readonly id: string;
+	readonly title: string;
+	readonly help?: string;
+}
