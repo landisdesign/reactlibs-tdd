4d89e4e2f0e9b5155f9d7f7b899067617f31a509
Michael Landis <michael4css@gmail.com>
2020-01-09 13:22:32 -0800

    Added arraysEqual function

--- src/common/index.test.ts
+++ src/common/index.test.ts
@@ -1,12 +1,67 @@
 import * as Common from './index';
 
-interface TestObject {
-    foo?: string;
-    bar?: boolean;
-    baz?: number;
-}
+describe('arraysEqual', () => {
+    test('simple comparison', () => {
+        let testA: any[] = [];
+        let testB: any[] = [];
+        let actual = Common.arraysEqual(testA, testB);
+        expect(actual).toBe(true);
+
+        testA = ['a'];
+        testB = ['a'];
+        actual = Common.arraysEqual(testA, testB);
+        expect(actual).toBe(true);
+
+        testA = [];
+        testB = ['a'];
+        actual = Common.arraysEqual(testA, testB);
+        expect(actual).toBe(false);
+
+        testA = ['a'];
+        testB = ['b'];
+        actual = Common.arraysEqual(testA, testB);
+        expect(actual).toBe(false);
+
+        testA = ['a'];
+        testB = ['a', 'a'];
+        actual = Common.arraysEqual(testA, testB);
+        expect(actual).toBe(false);
+
+        testA = ['a', 'a'];
+        testB = ['a', 'b'];
+        actual = Common.arraysEqual(testA, testB);
+        expect(actual).toBe(false);
+
+        testA = ['a', 'a'];
+        testB = ['a', 'a'];
+        actual = Common.arraysEqual(testA, testB);
+        expect(actual).toBe(true);
+    });
+
+    test('complex comparison', () => {
+        interface Foo {
+            foo: string;
+        }
+
+        let testA:Foo[] = [{foo: 'a'}];
+        let testB:Foo[] = [{foo: 'a'}];
+        let actual = Common.arraysEqual(testA, testB, (a, b) => a.foo === b.foo);
+        expect(actual).toBe(true);
+
+        testA = [{foo: 'a'}];
+        testB = [{foo: 'b'}];
+        actual = Common.arraysEqual(testA, testB, (a, b) => a.foo === b.foo);
+        expect(actual).toBe(false);
+    });
+});
 
 describe('assign', () => {
+
+    interface TestObject {
+        foo?: string;
+        bar?: boolean;
+        baz?: number;
+    }
 
     test('Single object returns object', () => {
         const expected: TestObject = {
--- src/common/index.ts
+++ src/common/index.ts
@@ -1,4 +1,16 @@
 
+export interface BinaryPredicate<T> {
+    (a: T, b: T): boolean;
+}
+
+
+export const arraysEqual = <T extends any>(a: T[], b: T[], test: BinaryPredicate<T> = (a, b) => a === b): boolean => {
+    if (a.length !== b.length) {
+        return false;
+    }
+    return a.every((x, i) => test(x, b[i]));
+};
+
 export const assign = <T, K extends keyof T>(arg0: T, ...args: T[]): T =>
     args.reduce( (result, current) =>
         (Object.keys(current) as K[]).reduce((target, key) => {
