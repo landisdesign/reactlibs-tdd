e4f62bfda87448e5f866b3a83f2aa7ac90aff6d4
Michael Landis <michael4css@gmail.com>
2020-01-09 10:39:09 -0800

    Initial reducer test definitions

---
+++ src/reducers/config/index.test.ts
@@ -0,0 +1,8 @@
+
+test.todo('populates initial state when no state is provided');
+test.todo('populates config URLs on LOAD_CONFIG');
+test.todo('replaces proper word config on LOAD_WORD');
+test.todo('replaces story config on LOAD_STORIES');
+test.todo('indicates state is loaded on RECONCILE_CONFIG');
+test.todo('indicates state is finished loading on APPLICATION_READY');
+test.todo('passes through existing state unchanged when unrecognized action provided');
