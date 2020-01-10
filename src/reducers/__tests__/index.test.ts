8686c07189fa303eb7395951dcb558cd9a0360a5
Michael Landis <michael4css@gmail.com>
2020-01-10 15:59:46 -0800

    Created tests for upfactoring Strategy reducer

---
+++ src/reducers/__tests__/index.test.ts
@@ -0,0 +1,3 @@
+test.todo('Method registered with reducer for provided action is used');
+test.todo('Action not registered with reducer returns provided state object');
+test.todo('Error action updates state with error message');
