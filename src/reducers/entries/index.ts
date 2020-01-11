ff6173fbb930e550653f3ffb07b3134ff471e55c
Michael Landis <michael4css@gmail.com>
2020-01-10 18:09:10 -0800

    Implemented entries reducer

---
+++ src/reducers/entries/index.ts
@@ -0,0 +1,40 @@
+import { StateConverterMap, createReducer } from "..";
+import { EntryAction, SET_ENTRY, SET_ENTRIES, CLEAR_ENTRIES, INIT_ENTRIES } from "./actions";
+
+export type EntryState = string[];
+
+const initialState: EntryState = [];
+
+const cloneState = (state: EntryState) : EntryState => [...state];
+
+const converters: StateConverterMap<EntryState, EntryAction> = {
+    [SET_ENTRY]: convertSetEntry,
+    [SET_ENTRIES]: convertSetEntries,
+    [CLEAR_ENTRIES]: convertClearEntries,
+    [INIT_ENTRIES]: convertInitEntries
+};
+
+export const entries = createReducer(initialState, cloneState, converters);
+
+function convertSetEntry(state: EntryState, action: EntryAction): EntryState {
+    const {
+        entry,
+        index
+    } = action.payload;
+
+    let newState = [...state];
+    newState[index] = entry;
+    return newState;
+}
+
+function convertSetEntries(state: EntryState, action: EntryAction): EntryState {
+    return [...action.payload];
+}
+
+function convertClearEntries(state: EntryState, action: EntryAction): EntryState {
+    return (new Array(state.length)).fill('');
+}
+
+function convertInitEntries(state: EntryState, action: EntryAction): EntryState {
+    return (new Array(action.payload)).fill('');
+}
