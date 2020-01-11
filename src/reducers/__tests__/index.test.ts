bc03dcfcdb0500d33d0d256983cf1680f062a0b3
Michael Landis <michael4css@gmail.com>
2020-01-10 16:41:36 -0800

    Created HOF for createing reducers

--- src/reducers/__tests__/index.test.ts
+++ src/reducers/__tests__/index.test.ts
@@ -1,4 +1,93 @@
-test.todo('createReducer returns reducer associated with provided Strategies');
-test.todo('Method registered with reducer for provided action is used');
-test.todo('Action not registered with reducer returns provided state object');
-test.todo('Error action updates state with error message');
+import { BaseAction, StateConverterMap, createReducer } from "..";
+
+interface TestState {
+    foo: string;
+    bar: string[];
+    baz: number;
+    error?: any;
+}
+
+const initialState: TestState = {
+    foo: 'a',
+    bar: ['x', 'y', 'z'],
+    baz: 25
+}
+
+const cloneState = (state: TestState): TestState => ({
+    ...state,
+    bar: [...state.bar]
+});
+
+const TEST_ACTION = 'TEST_ACTION';
+
+interface TestAction extends BaseAction {
+    payload: string;
+}
+
+const buildAction = (foo: string): TestAction => ({
+    type: TEST_ACTION,
+    payload: foo
+});
+
+const converterMap: StateConverterMap<TestState, TestAction> = {
+    [TEST_ACTION]: (state: TestState, action: TestAction): TestState => ({
+        ...cloneState(state),
+        foo: action.payload
+    })
+};
+
+test('createReducer returns function for future tests', () => {
+    const reducer = createReducer(initialState, cloneState, converterMap);
+
+    expect(typeof reducer).toEqual('function');
+});
+
+test('Method registered with reducer for provided action is used', () => {
+    const reducer = createReducer(initialState, cloneState, converterMap);
+    const action = buildAction('test');
+
+    const expected = {
+        ...cloneState(initialState),
+        foo: 'test'
+    };
+    const actual = reducer(initialState, action);
+
+    expect(actual).toEqual(expected);
+    // Intentionally not testing for equality here.
+    // It is the converter's responsibility to clone the state.
+});
+
+test('Action not registered with reducer returns provided state object', () => {
+    const reducer = createReducer(initialState, cloneState, converterMap);
+    const action = {type: 'FOO'};
+
+    const actual = reducer(initialState, action as TestAction);
+    expect(actual).toBe(initialState);
+});
+
+test('Initial state returned when no state provided', () => {
+    const reducer = createReducer(initialState, cloneState, converterMap);
+    const action = {type: 'FOO'};
+
+    const actual = reducer(<unknown>undefined as TestState, action as TestAction);
+
+    expect(actual).toBe(initialState);
+});
+
+test('Error action updates state with error message', () => {
+    const reducer = createReducer(initialState, cloneState, converterMap);
+    const action = {
+        ...buildAction('test'),
+        error: true,
+        payload: 'error'
+    };
+
+    const expected = {
+        ...cloneState(initialState),
+        error: action.payload
+    };
+
+    const actual = reducer(initialState, action);
+
+    expect(actual).toEqual(expected);
+});
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -4,6 +4,39 @@ export interface StateConverter<S, A extends BaseAction> {
     (source: S, action: A): S;
 }
 
+export interface StateConverterMap<S, A extends BaseAction> {
+    [index: string]: StateConverter<S, A>;
+}
+
+export interface Reducer<S, A extends BaseAction> {
+    (state: S, action: A): S;
+}
+
+export const createReducer = <S, A extends BaseAction>(
+    initialState: S,
+    cloneState: (state: S) => S,
+    converters: StateConverterMap<S,A>
+): Reducer<S,A> =>
+
+    (state: S = initialState, action: A): S => {
+        const converter = converters[action.type];
+        if (converter) {
+            if (action.error) {
+                return {
+                    ...cloneState(state),
+                    error: action.payload
+                };
+            }
+            else {
+                return converter(state, action);
+            }
+        }
+        else {
+            return state;
+        }
+    }
+;
+
 export const DEFAULT_ACTION_TYPE = '__default__';
 
 export interface ReduxState {
