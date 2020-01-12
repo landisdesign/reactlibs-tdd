2eecb62d1d0ed90079326e8abd090a8561a93fe8
Michael Landis <michael4css@gmail.com>
2020-01-11 16:20:41 -0800

    Implement tests and reducer

--- src/reducers/stories/__tests__/index.test.ts
+++ src/reducers/stories/__tests__/index.test.ts
@@ -1,3 +1,80 @@
-test.todo('Initial state returned when no state provided');
-test.todo('Provided state passed throug when bogus action provided');
-test.todo('Stories properly initialized');
+import { StoriesState, stories } from "..";
+import { InitStoriesAction, initStories } from "../actions";
+
+test('Initial state returned when no state provided', () => {
+    const expected: StoriesState = {
+        idMap: {},
+        stories: []
+    };
+    const bogusAction = {
+        type: 'FOO'
+    };
+
+    const actual = stories(undefined, bogusAction as InitStoriesAction);
+
+    expect(actual).toEqual(expected);
+    expect(actual).not.toBe(expected);
+});
+
+test('Provided state passed through when bogus action provided', () => {
+    const expected: StoriesState = {
+        idMap: {'a': 0},
+        stories: [
+            {
+                id: 'a',
+                title: 'A',
+                fields: ['a1'],
+                template: 'A'
+            }
+        ]
+    };
+
+    const bogusAction = {
+        type: 'FOO'
+    };
+
+    const actual = stories(expected, bogusAction as InitStoriesAction);
+
+    expect(actual).toBe(expected);
+});
+
+test('Stories properly initialized', () => {
+    const initial: StoriesState = {
+        idMap: {},
+        stories: []
+    };
+
+    const storyList = [
+        {
+            id: 'a',
+            title: 'A',
+            fields: ['a1', 'a2'],
+            template: 'A'
+        },
+        {
+            id: 'b',
+            title: 'B',
+            fields: ['b1', 'b2'],
+            template: 'B'
+        }
+    ];
+
+    const expected: StoriesState = {
+        idMap: {
+            'a': 0,
+            'b': 1
+        },
+        stories: storyList
+    };
+
+    const action = initStories(storyList);
+    const actual = stories(initial, action);
+
+    expect(actual).toEqual(expected);
+    expect(actual).not.toBe(expected);
+    expect(actual.stories).not.toBe(expected.stories);
+    actual.stories.forEach((story, index) => {
+        expect(story).not.toBe(expected.stories[index]);
+        expect(story.fields).not.toBe(expected.stories[index].fields);
+    });
+});
---
+++ src/reducers/stories/index.ts
@@ -0,0 +1,43 @@
+import { Story } from "../config/state";
+import { StateConverterMap, createReducer } from "..";
+import { StoriesAction, INIT_STORIES } from "./actions";
+
+interface IDMap {
+    [index: string]: number;
+}
+
+interface MutableStoriesState {
+    idMap: IDMap;
+    stories: Story[];
+}
+
+export interface StoriesState {
+    readonly idMap: Readonly<IDMap>;
+    readonly stories: Readonly<Story>[];
+}
+
+const initialState: StoriesState = {
+    idMap: {},
+    stories: []
+};
+
+const cloneState = (state: StoriesState) : StoriesState => ({
+    idMap: {...state.idMap},
+    stories: state.stories.map(story => ({...story, field: [...story.fields]}))
+});
+
+const converters:StateConverterMap<StoriesState, StoriesAction> = {
+    [INIT_STORIES]: convertInitStories
+}
+
+export const stories = createReducer(initialState, cloneState, converters);
+
+function convertInitStories(state: StoriesState, action: StoriesAction): StoriesState {
+    const newState: MutableStoriesState = action.payload.reduce((mutableState, story, index) => {
+        mutableState.idMap[story.id] = index;
+        mutableState.stories[index] = {...story, fields: [...story.fields]};
+        return mutableState;
+    }, ({idMap:{}, stories: new Array(action.payload.length)} as MutableStoriesState));
+
+    return newState;
+}
