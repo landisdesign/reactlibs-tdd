7197224481e62947208a0361e0aebb678cb733e4
Michael Landis <michael4css@gmail.com>
2020-01-11 16:59:49 -0800

    UI reducer tests outlined

---
+++ src/reducers/ui/__tests__/index.test.ts
@@ -0,0 +1,8 @@
+test.todo('Initial state returned when state not provided');
+test.todo('Provided state returned unchanged with bogus action');
+test.todo('Random updated');
+test.todo('Story Index updated');
+test.todo('Show Story updated');
+test.todo('Output updated');
+test.todo('Show Email and Transition Email updated');
+test.todo('Will Clear updated');
