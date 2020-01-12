aaf22f4df833b30269dc3fa7bfe5b6dea7f5a466
Michael Landis <michael4css@gmail.com>
2020-01-11 16:33:39 -0800

    Outline ui action test

---
+++ src/reducers/ui/__tests__/actions.test.ts
@@ -0,0 +1,6 @@
+test.todo('SetRandomAction populated');
+test.todo('SetStoryIndex populated');
+test.todo('SetShowStory populated');
+test.todo('SetOutputAction populated');
+test.todo('SetShowEmailAction populated');
+test.todo('SetWillClearAction populated');
