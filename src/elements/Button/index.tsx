b109838dc51db75858b4c54757364768aff01301
Michael Landis <michael4css@gmail.com>
2020-01-13 14:14:58 -0800

    Cleanup ButtonProps interface

--- src/elements/Button/index.tsx
+++ src/elements/Button/index.tsx
@@ -8,12 +8,13 @@ import styles from './Button.module.scss';
  *  @property render If provided, overrides `children` as the content for the button
  *  @property className if provided, overrides default styling
  */
-export interface ButtonProps {
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     isDefault?: boolean;
     render?: () => React.ReactNode;
+    className?: string;
 }
 
-const Button: React.FC<ButtonProps & React.ButtonHTMLAttributes<HTMLButtonElement>> = (props) => {
+const Button: React.FC<ButtonProps> = (props) => {
     const {
         isDefault = false,
         className = styles[isDefault ? 'default' : 'button'],
