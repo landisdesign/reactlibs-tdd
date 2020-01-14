a47c67856ab4a0bcf22324511dd8d2e08a8f09e8
Michael Landis <michael4css@gmail.com>
2020-01-13 16:29:01 -0800

    Fix style tests

--- src/elements/ProgressIndicator/index.test.tsx
+++ src/elements/ProgressIndicator/index.test.tsx
@@ -9,28 +9,21 @@ test('HTML structure is proper', () => {
     }
     const expected = <div className='progressIndicator' style={expectedStyle}/>;
     const wrapper = shallow(<ProgressIndicator current={0} total={100}/>);
-
     expect(wrapper.matchesElement(expected)).toEqual(true);
 });
 
 test('Background styled properly for current/total values', () => {
-    let expected: React.CSSProperties = {
-        background: '#333'
-    };
-    let wrapper = shallow(<ProgressIndicator current={1} total={100} backgroundColor='#333'/>);
-    expect(wrapper.prop('style')).toEqual(expected);
+    let expected = '#333';
+    let wrapper = shallow(<ProgressIndicator current={1} total={1000} backgroundColor='#333'/>);
+    expect(wrapper.prop('style').background).toEqual(expected);
 
-    expected = {
-        background: '#CCC'
-    };
-    wrapper = shallow(<ProgressIndicator current={99} total={100} progressColor='#CCC'/>);
-    expect(wrapper.prop('style')).toEqual(expected);
+    expected = '#CCC';
+    wrapper = shallow(<ProgressIndicator current={999} total={1000} progressColor='#CCC'/>);
+    expect(wrapper.prop('style').background).toEqual(expected);
 
-    expected = {
-        background: 'linear-gradient(to right, #369 49.9%, #FFF 50.1%)'
-    };
+    expected = 'linear-gradient(to right, #369 49.9%, #FFF 50.1%)';
     wrapper = shallow(<ProgressIndicator current={50} total={100}/>);
-    expect(wrapper.prop('style')).toEqual(expected);
+    expect(wrapper.prop('style').background).toEqual(expected);
 });
 
 test('Width received', () => {
@@ -40,29 +33,25 @@ test('Width received', () => {
 });
 
 test('Current/Total clamped properly', () => {
-    let expected: React.CSSProperties = {
-        background: '#369'
-    };
+    let expected = '#369';
 
     let wrapper = shallow(<ProgressIndicator current={200} total={100}/>);
-    expect(wrapper.prop('style')).toEqual(expected);
+    expect(wrapper.prop('style').background).toEqual(expected);
 
     wrapper = shallow(<ProgressIndicator current={1} total={0}/>);
-    expect(wrapper.prop('style')).toEqual(expected);
+    expect(wrapper.prop('style').background).toEqual(expected);
 
     wrapper = shallow(<ProgressIndicator current={1} total={-1}/>);
-    expect(wrapper.prop('style')).toEqual(expected);
+    expect(wrapper.prop('style').background).toEqual(expected);
 
-    expected = {
-        background: '#FFF'
-    };
+    expected = '#FFF';
 
     wrapper = shallow(<ProgressIndicator current={-1} total={5}/>);
-    expect(wrapper.prop('style')).toEqual(expected);
+    expect(wrapper.prop('style').background).toEqual(expected);
 
     wrapper = shallow(<ProgressIndicator current={0} total={-1}/>);
-    expect(wrapper.prop('style')).toEqual(expected);
+    expect(wrapper.prop('style').background).toEqual(expected);
 
     wrapper = shallow(<ProgressIndicator current={-1} total={-1}/>);
-    expect(wrapper.prop('style')).toEqual(expected);
+    expect(wrapper.prop('style').background).toEqual(expected);
 });
