29b8ef6c494d5d1a9929fa322c688cda5bb3ca06
Michael Landis <michael4css@gmail.com>
2020-01-13 18:05:56 -0800

    Update test to reflect innerHTML override

--- src/elements/Text/index.test.tsx
+++ src/elements/Text/index.test.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import Text from '.';
 
-test.todo('Children are output', () => {
+test('Children are output', () => {
     const content = <b>Hello</b>;
-    const expected = <div>{content}</div>;
+    const expected = <div className='default'>{content}</div>;
     const wrapper = shallow(<Text>{content}</Text>);
 
     expect(wrapper.matchesElement(expected)).toEqual(true);
 });
 
-test.todo('HTML is output', () => {
-    const content = '<b>Hello</b>';
-    const expected = <div><b>Hello</b></div>;
-    const wrapper = shallow(<Text html={content}/>);
+test('HTML is output dangerously', () => {
+    let tree = renderer.create(<Text html='<b>Hello</b>' />).toJSON();
+    expect(tree).toMatchSnapshot();
 
-    expect(wrapper.matchesElement(expected)).toEqual(true);
+    tree = renderer.create(<Text html='<b>Hello</b>'>This is not rendered</Text>).toJSON();
+    expect(tree).toMatchSnapshot();
 });
 
-test.todo('Styles are applied', () => {
+test('Styles are applied', () => {
     const content = <b>Hello</b>;
     const expected = <div className='story'>{content}</div>;
     const wrapper = shallow(<Text type='story'>{content}</Text>);
--- src/elements/Text/index.tsx
+++ src/elements/Text/index.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
+import styles from './Text.module.scss';
 
 const textTypes = ['story'] as const;
 
+/**
+ * Defines properties for Text component
+ * @property type If provided, identifies the styling for this component.
+ * Currently only `story` is an option.
+ * @property html If provided, overrides the children of this component with any
+ * content. **This will render any text as HTML, including unsafe scripts.**
+ */
 export interface TextProps {
     type?: typeof textTypes[number];
     html?: string;
 }
 
 const Text: React.FC<TextProps> = (props) => {
-    return null;
+    const {
+        type = 'default',
+        html,
+        children
+    } = props;
+
+    const className = styles[type] ?? styles.default;
+
+    return html
+        ? <div className={className} dangerouslySetInnerHTML={{ __html: html }} />
+        : <div className={className}>{children}</div>
+    ;
 }
 
 export default Text;
